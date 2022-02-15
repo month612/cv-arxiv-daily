41bd0bf7c945c267f2ae245f0b76b1e316251f1e
month612 <862606739@qq.com>
2022-02-15 21:57:10 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -196,7 +196,7 @@ if __name__ == "__main__":
     keywords["Visual Localization"] = "\"Camera Localization\"OR\"Visual Localization\"OR\"Camera Re-localisation\"OR\"Loop Closure Detection\"OR\"visual place recognition\""
     keywords["Keypoint Detection"]  = "\"Keypoint Detection\"OR\"Feature Descriptor\""
     keywords["Image Matching"]      = "\"Image Matching\""
-    keywords["Deep Learning"]      = "\"Deep Learning\"+"OR"+"\"Deep\"+"OR"+"\"Deep VO\"+"OR"+"\"Deep Visual Odometry\""
+    keywords["Deep Learning"]      = "\"Deep Learning\"OR\"Deep\"OR\"Deep VO\"OR\"Deep Visual Odometry\""
     keywords["Visual Odometry"]      = "\"Visual Odometry\""
 
     for topic,keyword in keywords.items():
